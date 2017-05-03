182f1cfd3c56c5fd4821f41c313deabe3c127906
Ross William Parsons 140 <rwp140@gmail.com>
2017-05-03 00:59:56 -0600

    Added ListItem directive .js

--- app/modules/ToDo/SubDirectives/listItem/listItem.directive.js
+++ app/modules/ToDo/SubDirectives/listItem/listItem.directive.js
@@ -1,4 +1,26 @@
 (function(){
   'use strict'
-  
+
+  angular.module('ToDo')
+  .directive('listItem',listItem)
+
+  //sample.$inject = ['ToDoService'];
+  function listItem (){
+    return {
+      restrict: 'E',
+      scope: {
+        placeholder: "@",
+        list: "=",
+        property: "@"
+      },
+      templateUrl: 'modules/ToDo/SubDirectives/listItem/listItem.directive.html',
+      link: function(scope) {
+        function init(){
+
+        }
+
+        init();
+      }
+    };
+  }
 })();
