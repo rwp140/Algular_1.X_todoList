9db76fe9f25d03a5ff9db359e969e0b47401f78a
Ross William Parsons 140 <rwp140@gmail.com>
2017-05-05 22:21:05 -0600

    Established firebase.service.js

--- app/modules/app.js
+++ app/modules/app.js
@@ -4,6 +4,7 @@
   // Declare app level module which depends on views, and components
   angular.module('myApp', [
     'ngRoute',
+    'services',
     'ToDo'
   ]).
   config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
