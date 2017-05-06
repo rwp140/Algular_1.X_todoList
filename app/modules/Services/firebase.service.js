056693b3e67b262f6d015c94eacd86c0073b6bd9
Ross William Parsons 140 <rwp140@gmail.com>
2017-05-05 22:21:28 -0600

    established firbase service

---
+++ app/modules/Services/firebase.service.js
@@ -0,0 +1,19 @@
+(function() {
+	'use strict';
+
+  angular.module('services')
+		.service('firebaseService', firebaseService);
+
+	firebaseService.$inject = [];
+	function firebaseService() {
+		var service = this;
+    /*====================== Delegation Variables =========================== */
+    /*====================== public Variables =============================== */
+    /*====================== private Variables ============================== */
+    /*====================== Services ======================================= */
+    /*====================== Public Methods ================================= */
+    /*====================== Private Methods ================================ */
+    /*====================== Actions ======================================== */
+	}
+
+})();
