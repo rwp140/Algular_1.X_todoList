84558b27920ae3b4cb6253187e2c038ef4248bc0
Ross William Parsons 140 <rwp140@gmail.com>
2017-05-08 17:41:40 -0600

    injected profile services, and connected data interactions

--- app/modules/ToDo/SubDirectives/Profile/profile.directive.js
+++ app/modules/ToDo/SubDirectives/Profile/profile.directive.js
@@ -4,15 +4,19 @@
   angular.module('ToDo')
   .directive('profile',profile);
 
-  //sample.$inject = ['ToDoService'];
-  function profile (){
+  profile.$inject = ['profileService'];
+  function profile (profileService){
     return {
       restrict: 'E',
       scope: true,
       templateUrl: 'modules/ToDo/SubDirectives/profile/profile.directive.html',
       link: function(scope) {
+        //var dir = this;
+        var psvc = profileService;
+        scope.psvc = psvc;
+        //scope.dir = this;
         function init(){
-
+          //console.log(dir.psvc.profileMode);
         }
 
         init();
