e3e8bd0eec84b7e7f50c069ec6d16d014d7ddbd6
Ross William Parsons 140 <rwp140@gmail.com>
2017-06-14 03:26:22 -0600

    alphabitatized functions

--- app/modules/Services/firebase.service.js
+++ app/modules/Services/firebase.service.js
@@ -150,36 +150,13 @@
       });
       return deferred.promise;
     }
-    function signUpUser(email,password){
-      var deferred = $q.defer();
-      //note you can do .then(resolution/success,resolution/success instead of  .then(resolution/success).catch(resolution/success)
-      auth.createUserWithEmailAndPassword(email, password)
-      .then(function(user){
-        emailConfirm(user);
-        deferred.resolve(user);
-      }).catch(function(error) {
-        // Handle Errors here.
-        //var errorCode = error.code;
-        //var errorMessage = error.message;
-        // [START_EXCLUDE]
-        if (error.code == 'auth/weak-password') {
-         alert('The password is too weak.');
-        } else {
-         alert(error.message);
-        }
-        deferred.reject(error);
-        //console.log(error);
-        // [END_EXCLUDE]
-      });
-
-      return deferred.promise;
-    }
     function signInUser(email,password){
       var deferred = $q.defer();
       //note you can do .then(resolution/success,resolution/success instead of  .then(resolution/success).catch(resolution/success)
       auth.signInWithEmailAndPassword(email, password)
       .then(function(user){
         //console.log("then succes");
+        console.log(user);
         deferred.resolve(user);
       })
       .catch(function(error) {
@@ -199,6 +176,24 @@
       auth.signOut().then(function(){
         deferred.resolve(true);
       }).catch(function(error){
+        deferred.reject(error);
+      });
+
+      return deferred.promise;
+    }
+    function signUpUser(email,password){
+      var deferred = $q.defer();
+      //note you can do .then(resolution/success,resolution/success instead of  .then(resolution/success).catch(resolution/success)
+      auth.createUserWithEmailAndPassword(email, password)
+      .then(function(user){
+        emailConfirm(user);
+        deferred.resolve(user);
+      }).catch(function(error) {
+        if (error.code == 'auth/weak-password') {
+         alert('The password is too weak.');
+        } else {
+         alert(error.message);
+        }
         deferred.reject(error);
       });
 
